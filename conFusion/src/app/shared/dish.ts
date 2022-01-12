e171aaed4d7569f2de6f9f90a62ceda51061ed36
Matheus Montanari <matheusrmontanari@gmail.com>
2022-01-12 14:22:46 -0300

    Git Components part 2

--- conFusion/src/app/shared/dish.ts
+++ conFusion/src/app/shared/dish.ts
@@ -1,10 +1,10 @@
 export class Dish {
-    id!: string;
-    name!: string;
-    image!: string;
-    category!: string;
-    featured!: boolean;
-    label!: string;
-    price!: string;
-    description!: string;
+    id?: string; // ponto d einterrogação para tornar a propriedade opcional e não ter erro de inicialização
+    name?: string;
+    image?: string;
+    category?: string;
+    featured?: boolean;
+    label?: string;
+    price?: string;
+    description?: string;
     }
